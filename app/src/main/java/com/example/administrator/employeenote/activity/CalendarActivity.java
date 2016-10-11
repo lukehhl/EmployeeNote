0111fc34415b92e1297b51bbc07d82bd9ac6dfa6
ge11522 <x_ganzhe@hotmail.com>
2016-10-11 17:04:17 +0800

    添加了CalendarActivity页面

--- app/src/main/java/com/example/administrator/employeenote/activity/CalendarActivity.java
+++ app/src/main/java/com/example/administrator/employeenote/activity/CalendarActivity.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 
 import com.example.administrator.employeenote.R;
 
+import java.util.Calendar;
+
 public class CalendarActivity extends AppCompatActivity {
+
+    private Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
