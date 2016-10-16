f960936a6e34190febd61d01666ede353be12b9f
luke <x_ganzhe@hotmail.com>
2016-10-16 23:19:06 +0800

    加入了calendarview Library

--- app/src/main/java/com/example/administrator/employeenote/activity/CalendarActivity.java
+++ app/src/main/java/com/example/administrator/employeenote/activity/CalendarActivity.java
@@ -2,18 +2,37 @@ package com.example.administrator.employeenote.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 import com.example.administrator.employeenote.R;
+import com.github.tibolte.agendacalendarview.AgendaCalendarView;
 
 import java.util.Calendar;
 
 public class CalendarActivity extends AppCompatActivity {
 
-    private Calendar calendar;
+    private CalendarView calendar;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        initView();
+    }
+
+    private void initView() {
+        calendar = (CalendarView) findViewById(R.id.calendar_view);
+        text = (TextView) findViewById(R.id.text);
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                view.setBackgroundColor(getResources().getColor(R.color.red));
+                text.setText(String.valueOf(dayOfMonth));
+            }
+        });
+
     }
 }
